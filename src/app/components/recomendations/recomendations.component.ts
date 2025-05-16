8f035d4c2aac2e24085fb51a3570f28020fadf63
carlos.garcia <carlosgarciamaqueda.soltel@gmail.com>
2025-05-16 11:49:13 +0200

    Ajustes de accesiblidad

--- src/app/components/recomendations/recomendations.component.ts
+++ src/app/components/recomendations/recomendations.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { VolverComponent } from "../../modules/volver/volver.component";
 
 @Component({
   selector: 'app-recomendations',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, VolverComponent],
   templateUrl: './recomendations.component.html',
   styleUrl: './recomendations.component.scss'
 })
 export class RecomendationsComponent {
   musculos = ['ESPALDA','PECHO', 'HOMBROS', 'TRICEPS', 'BICEPS', 'ANTEBRAZOS', 'LUMBARES', 'ABDOMINALES', 'PIERNAS'];
-    musculoSeleccionado = 'Espalda';
-    nivelSeleccionado: string = "";
+  musculoSeleccionado = 'Espalda';
+  
 
-    seleccionarMusculo(musculo: string): void {
-      this.musculoSeleccionado = musculo;
-    }
+  constructor(private route: ActivatedRoute, private router: Router) { }  
+
+  seleccionarMusculo(musculo: string): void {
+    this.musculoSeleccionado = musculo;
+  }
+
+  volver() {
+    window.history.back(); // Navega a la página anterior
+  }
+
+  irPerfiloLogin(){
+    //Logica con firebase si se ha autenticado
+  }
 }
