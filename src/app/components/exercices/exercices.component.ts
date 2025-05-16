f18d00bad458922e3d54db74099e80809ca68c28
carlos.garcia <carlosgarciamaqueda.soltel@gmail.com>
2025-05-16 10:34:43 +0200

    reajustes colores, tamaños

--- src/app/components/exercices/exercices.component.ts
+++ src/app/components/exercices/exercices.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { VolverComponent } from "../../modules/volver/volver.component";
 
 @Component({
   selector: 'app-exercices',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, VolverComponent],
   templateUrl: './exercices.component.html',
   styleUrl: './exercices.component.scss'
 })
@@ -12,9 +13,9 @@ export class ExercicesComponent implements OnInit {
   musculos = ['ESPALDA','PECHO', 'HOMBROS', 'TRICEPS', 'BICEPS', 'ANTEBRAZOS', 'LUMBARES', 'ABDOMINALES', 'PIERNAS'];
   musculoSeleccionado = 'Espalda';
   nivelSeleccionado: string = "";
-  // Eliminar la propiedad 'router: any' porque no se está utilizando de esta manera.
+  
 
-  constructor(private route: ActivatedRoute, private router: Router) { }  // Inyección del Router aquí
+  constructor(private route: ActivatedRoute, private router: Router) { }  
 
   ngOnInit(): void {
     this.nivelSeleccionado = this.route.snapshot.paramMap.get('level')!;
@@ -27,6 +28,14 @@ export class ExercicesComponent implements OnInit {
 
   seleccionarMusculo(musculo: string): void {
     this.musculoSeleccionado = musculo;
+  }
+
+  volver() {
+    window.history.back(); // Navega a la página anterior
+  }
+
+  irPerfiloLogin(){
+    //Logica con firebase si se ha autenticado
   }
 }
 
