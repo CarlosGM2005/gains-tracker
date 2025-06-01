f85eac2cef4c28084b6459019693b6137b009358
Carlos.Garcia <carlosgarmaq2005@gmail.com>
2025-06-01 10:54:31 +0200

    Ajustes

--- src/app/components/exercices/exercices.component.ts
+++ src/app/components/exercices/exercices.component.ts
@@ -29,6 +29,7 @@ export class ExercicesComponent implements OnInit {
     }
   }
 
+  //Llamar al servicio encargado de traerme los datos de los ejercicios
   seleccionarMusculo(musculo: string): void {
     this.musculoSeleccionado = musculo;
   }
@@ -51,72 +52,72 @@ export class ExercicesComponent implements OnInit {
     {
       nombre: 'Elevaciones laterales con mancuernas',
       series: 4,
-      imagen: 'https://placehold.co/67x67'
+      imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Press militar con barra',
       series: 3,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Remo con mancuerna',
       series: 5,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Curl de bíceps con barra',
       series: 3,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Extensión de tríceps en polea',
       series: 4,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Sentadillas con barra',
       series: 5,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Peso muerto rumano',
       series: 4,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Press banca',
       series: 3,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Pull-over con mancuerna',
       series: 3,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Face pulls',
       series: 4,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Zancadas con mancuernas',
       series: 3,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Press Arnold',
       series: 3,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Remo en polea baja',
       series: 4,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     },
     {
       nombre: 'Dominadas asistidas',
       series: 3,
-      imagen: 'https://placehold.co/67x67'
+       imagenInicio: 'https://placehold.co/67x67'
     }
   ];
 
